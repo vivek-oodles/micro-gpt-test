db6cae13d42cd9bc1610789b5846f7b49f2577cd
Levi <70198955+Draysongz@users.noreply.github.com>
2024-10-25 15:08:54 +0100

    done

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -7,7 +7,7 @@ interface UseUserLoginProps {
   loading: boolean;
 }
 
-const baseURL = 'https://888e-105-112-29-195.ngrok-free.app/api'
+const baseURL = 'https://b7d9-105-115-0-44.ngrok-free.app/api'
 
 // Custom Hook for handling user login
 export const useUserLogin = (initData: string, referralCode?: string): UseUserLoginProps => {
