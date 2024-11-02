d098d94abad48212ab0b98e5b9e61cf341057572
Levi <70198955+Draysongz@users.noreply.github.com>
2024-11-02 12:28:17 +0100

    dd

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import { useUserLogin } from "./hooks/useAuth";
 
 function App() {
-  //  const [telegramInitData, setTelegramInitData] = useState<string>("");
+   const [telegramInitData, setTelegramInitData] = useState<string>("");
    const queryString = window.location.search; // Get the query string
    const urlParams = new URLSearchParams(queryString);
    const referralId = urlParams.get("referralCode")!;
